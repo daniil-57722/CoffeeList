8f9389441a80a622f87e32e583a8935bf2b21227
Elizaveta9 <katkova_lizochka@bk.ru>
2021-10-15 19:09:22 +0300

    #2 переход с <string-array> на HashMap

--- app/src/main/java/com/example/coffeelist/MainActivity.java
+++ app/src/main/java/com/example/coffeelist/MainActivity.java
@@ -3,13 +3,41 @@ package com.example.coffeelist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    Map<String, String> coffeesMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        coffeesMap.put("Эспрессо", "Здесь могла бы быть информация про Эспрессо");
+        coffeesMap.put("Американо", "Здесь могла бы быть информация про Американо");
+        coffeesMap.put("Капучино", "Здесь могла бы быть информация про Капучино");
+        coffeesMap.put("Латте", "Здесь могла бы быть информация про Латте");
+        coffeesMap.put("Гляссе", "Здесь могла бы быть информация про Гляссе");
+        coffeesMap.put("Мокко", "Здесь могла бы быть информация про Мокко");
+        coffeesMap.put("Флэт Уайт", "Здесь могла бы быть информация про Флэт Уайт");
+        coffeesMap.put("Раф", "Здесь могла бы быть информация про Раф");
+        coffeesMap.put("Макиато", "Здесь могла бы быть информация про Макиато");
+        coffeesMap.put("Латте макиато", "Здесь могла бы быть информация про Латте макиато");
+
+        ListView coffeesListView = (ListView) findViewById(R.id.coffeesListView);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                (List) coffeesMap.keySet()
+        );
+
+        coffeesListView.setAdapter(arrayAdapter);
     }
 
 }
